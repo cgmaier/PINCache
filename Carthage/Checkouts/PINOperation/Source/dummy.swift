2722cc7b77309d0d21c506271eb3ab219c63c7c8
Christopher Maier <chrismaier@Christophers-MacBook-Pro.local>
2021-02-04 10:10:40 -0800

    add dummy swift files

---
+++ Carthage/Checkouts/PINOperation/Source/dummy.swift
@@ -0,0 +1,13 @@
+//
+//  dummy.swift
+//  PINOperation
+//
+//  Created by Christopher Maier on 2/4/21.
+//  Copyright © 2021 Pinterest. All rights reserved.
+//
+
+import Foundation
+
+class SwiftTest: NSObject {
+    var cache: PINOperationBlock?
+}
---
+++ Source/dummy.swift
@@ -0,0 +1,13 @@
+//
+//  dummy.swift
+//  PINCache
+//
+//  Created by Christopher Maier on 2/4/21.
+//  Copyright © 2021 Pinterest. All rights reserved.
+//
+
+import Foundation
+
+class SwiftTest: NSObject {
+    var cache: PINCache? 
+}
